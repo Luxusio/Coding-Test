e594dc1b16467eb84cc84c15d1df926c9f229842
Luxusio <44326048+Luxusio@users.noreply.github.com>
2021-04-12 08:03:12 +0900

    Solved L2 최솟값 만들기

---
+++ L2/L2_최솟값 만들기.cpp
@@ -0,0 +1,12 @@
+//// 2021.04.11 19:54
+//#include <vector>
+//#include <algorithm>
+//using namespace std;
+//
+//int solution(vector<int> A, vector<int> B) {
+//	sort(A.begin(), A.end());
+//	sort(B.begin(), B.end(), greater<int>());
+//	int sum = 0;
+//	for (int i = 0; i < A.size(); i++) sum += A[i] * B[i];
+//	return sum;
+//}
