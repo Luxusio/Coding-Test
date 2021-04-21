3642d7e2a4fc6d88c5e55add64d6727ca70e7456
Luxusio <44326048+Luxusio@users.noreply.github.com>
2021-04-21 10:43:24 +0900

    Solved L3 징검다리 건너기
    With binary search

--- L3/L3_징검다리 건너기.cpp
+++ L3/L3_징검다리 건너기.cpp
@@ -1,49 +1,26 @@
 #include <vector>
-#include <queue>
+#include <algorithm>
 using namespace std;
 
-struct compare {
-	bool operator()(pair<int, int> p1, pair<int, int> p2) {
-		return p1.second > p2.second;
-	}
-};
-
 int solution(vector<int> stones, int k) {
-	priority_queue<pair<int, int>, vector<pair<int, int>>, compare> heap;
-	vector<int> distance(stones.size(), 1);
-	vector<int> end(stones.size(), -1);
-	for (int i = 0; i < stones.size(); i++) heap.push({ i, stones[i] });
-	
-	while (!heap.empty()) {
-		pair<int, int> p = heap.top(); heap.pop();
+	int low = 0, high = 200000000;
+	while (low <= high) {
+		int mid = (low + high) / 2;
+		int max_len = 0;
+		int now_len = 0;
 
-		int rvalue = p.first < stones.size() - 1 ? distance[p.first + 1] : 1;
-		int lvalue = p.first > 0 ? distance[p.first - 1] : 1;
-		distance[p.first] = rvalue + lvalue;
-
-		if (lvalue > 1) {
-			if (rvalue > 1) { // both
-				int temp = end[p.first - 1];
-				end[p.first - 1] = end[p.first + 1];
-				end[p.first + 1] = temp;
-				distance[end[p.first - 1]] = distance[p.first];
-				distance[end[p.first + 1]] = distance[p.first];
-			}
-			else { // left only
-				end[p.first] = end[p.first - 1];
-				end[end[p.first - 1]] = p.first;
-				distance[end[p.first]] = distance[p.first];
+		for (int i = 0; i < stones.size(); i++) {
+			if (stones[i] <= mid) now_len++;
+			else {
+				max_len = max(now_len, max_len);
+				now_len = 0;
 			}
 		}
-		else if (rvalue > 1) { // right only
-			end[p.first] = end[p.first + 1];
-			end[end[p.first + 1]] = p.first;
-			distance[end[p.first]] = distance[p.first];
-		}
-		else end[p.first] = p.first;
+		max_len = max(now_len, max_len);
 
-		if (distance[p.first] > k) return p.second;
+		if (max_len >= k) high = mid - 1;
+		else low = mid + 1;
 	}
 
-	return 0;
+	return low;
 }
